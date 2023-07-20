85295de41997365824f7e34138c4a16ba7bba8e8
sonaw <sonaw@LAPTOP-2JJ2LORH>
2023-07-20 14:07:36 +0530

    First Commit

---
+++ dayone/src/org/tnsif/introduction/CompilerInput.java
@@ -0,0 +1,12 @@
+//Program to demonstrate on Compile Time Input
+package org.tnsif.introduction;
+
+public class CompilerInput {
+
+	public static void main(String[] args) {
+		//variable initialzation
+		short num=34;
+		System.out.println("The num is: "+num);
+	}
+
+}
---
+++ dayone/src/org/tnsif/introduction/UserInputDemo.java
@@ -0,0 +1,17 @@
+//Program to Demonstrate on User Input Value
+package org.tnsif.introduction;
+
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+	@SuppressWarnings("resource")
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the String :");
+	String str1=s.next();
+	System.out.println("The String :" +str1);
+	
+	}
+
+}
